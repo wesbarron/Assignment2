1448c95cce92ad9921e713f525d009657c529adc
wesbarron <barron_wesley@columbusstate.edu>
2019-09-24 22:14:58 +0000

    Changing content in static comic page.

--- template/template.php
+++ template/template.php
@@ -28,6 +28,9 @@
                     <li class="nav-item">
                     <a class="nav-link" href="index.php">Home</a>
                     </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="content/landing.php">About</a>
+                    </li>
                 </ul>
                 </nav>
                 <h1><?php site_name(); ?></h1>
