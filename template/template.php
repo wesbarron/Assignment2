e7fd05ec0d89d6d9a7d23d96880f28b43c56d636
wesbarron <barron_wesley@columbusstate.edu>
2019-09-24 20:13:04 +0000

    changing some css background styles.

--- template/template.php
+++ template/template.php
@@ -3,6 +3,7 @@
 <head>
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
     <link href="template/style.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
     <script>
     $(document).ready(function() {
