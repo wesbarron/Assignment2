c6dea3518a107c68daa20a965e351c9c87f41ce2
wesbarron <barron_wesley@columbusstate.edu>
2019-09-24 20:43:48 +0000

    playing with button in home.phtml.

--- template/template.php
+++ template/template.php
@@ -4,24 +4,32 @@
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
     <link href="template/style.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>  
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
     $(document).ready(function() {
         $("button").click(function() {
             $.get("template/comic.php", function(data, status) {
                 $("#comic").html(data);
-                
+
             })
         })
     })
-</script> 
-    
+</script>
+
 </head>
 <body>
-    
+
     <div class="wrap">
         <div id="header">
             <header>
+                <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+                <a class="navbar-brand" href="#">Logo</a>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="home.phtml">Home</a>
+                    </li>
+                </ul>
+                </nav>
                 <h1><?php site_name(); ?></h1>
             </header>
         </div>
@@ -33,7 +41,7 @@
                 <div id="comic">
                     <?php first_comic(); ?>
                     <br><br>
-  
+
                 </div>
             </article>
         </div>
