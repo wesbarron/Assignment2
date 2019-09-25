950f237d741892b62f7cc8282f5b0ae077359946
wesbarron <54125356+wesbarron@users.noreply.github.com>
2019-09-25 09:21:31 -0400

    Update template.php
    Added mobile view port to the template.

--- template/template.php
+++ template/template.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial scale=1">
     <link href="template/style.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
