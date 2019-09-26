cc5cb73480ae384c8a6699bbdb64d86e335cdb1e
wesbarron <barron_wesley@columbusstate.edu>
2019-09-26 18:25:03 +0000

    changed functions to make url dynamic.

--- template/comic.php
+++ template/comic.php
@@ -1,13 +1,14 @@
 <?php
-   get_comic();
-   /*
-    $original_url = 'http://xkcd.com/614/info.0.json';
+
+    //$original_url = 'http://xkcd.com/614/info.0.json';
+    $first_url = 'http://xkcd.com/';
+    $last_url = '/info.0.json';
     $random_number = rand(1,1220);
-    $new_url = substr_replace($original_url, $random_number, 16,-12);
+    $new_url = $first_url . $random_number . $last_url;//substr_replace($original_url, $random_number, 16,-12);
 
     $json = file_get_contents($new_url);
     $obj = json_decode($json);
     echo "<h1>".$obj->title."</h1>" . "<h2>Year Published: ".$obj->year."</h2>";
     echo "<img src='" . $obj->img . "' width='55%' height='15%' alt='Coming Soon!'>";
-    */
+
 ?>
