d307de11d72ddd5032e964ce6725f4f2371ff1cb
wesbarron <barron_wesley@columbusstate.edu>
2019-09-26 18:48:04 +0000

    renamed files in config file.

--- includes/functions.php
+++ includes/functions.php
@@ -66,7 +66,7 @@ function page_title()
  */
 function page_content()
 {
-    $page = isset($_GET['page']) ? $_GET['page'] : 'home';
+    $page = isset($_GET['page']) ? $_GET['page'] : 'Original Comic';
     $path = getcwd() . '/' . config('content_path') . '/' . $page . '.phtml';
     if (! file_exists($path)) {
         $path = getcwd() . '/' . config('content_path') . '/404.phtml';
