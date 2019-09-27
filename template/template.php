935e41cdae0da65229d29174420b79aa5eb8c330
wesbarron <barron_wesley@columbusstate.edu>
2019-09-27 03:29:37 +0000

    Trying to repair ajax function - stoped working.

--- template/template.php
+++ template/template.php
@@ -4,7 +4,7 @@
     <title><?php page_title(); ?> | <?php site_name(); ?></title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial scale=1">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">-->
     <link href="template/style.css" rel="stylesheet" type="text/css">
 
