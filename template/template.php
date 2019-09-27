55b13aaec99cd5c39668ededd661555a002f5e4f
wesbarron <barron_wesley@columbusstate.edu>
2019-09-27 04:16:55 +0000

    Trying to repair ajax function - stoped working.

--- template/template.php
+++ template/template.php
@@ -8,20 +8,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="template/style.css" rel="stylesheet" type="text/css">
 
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!--
-    <script>
-    $(document).ready(function() {
-        $("button").click(function() {
-            $.get("template/comic.php", function(data, status) {
-                $("#comic").html(data);
-                alert(status);
-
-            })
-        })
-    })
-</script>
--->
 </head>
 
 <body>
@@ -79,7 +65,20 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://use.fontawesome.com/f1c966bcb4.js"></script>
-       <script>
+
+        <script>
+    $(document).ready(function() {
+        $("button").click(function() {
+            $.get("template/comic.php", function(data, status) {
+                $("#comic").html(data);
+                alert(status);
+
+            })
+        })
+    });
+</script>
+
+       <!--<script>
       $('#comic-button').click(function (e){
           $.ajax({
               type: 'post',
@@ -97,6 +96,6 @@
           }); // Ajax close
           return false; // So the button click does not refresh the page
       }) // Function end
-    </script>
+    </script>-->
     </body>
 </html>
