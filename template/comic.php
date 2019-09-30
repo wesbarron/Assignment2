d1056e5f4da37216af7768e259a1faa18a89fa7a
wesbarron <barron_wesley@columbusstate.edu>
2019-09-30 14:19:01 +0000

    Experimenting with an event listener in the random comic page.

--- template/comic.php
+++ template/comic.php
@@ -8,7 +8,8 @@
 
     $json = file_get_contents($new_url);
     $obj = json_decode($json);
+    echo "<div id='change-comic'>";
     echo "<h1>".$obj->title."</h1>" . "<h2>Year Published: ".$obj->year."</h2>";
     echo "<img src='" . $obj->img . "' width='55%' height='15%' alt='Coming Soon!'>";
-
+    echo "</div>";
 ?>
